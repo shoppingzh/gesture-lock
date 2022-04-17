45ed333321bafa1e91c58a66f2492718e7e695af
shoppingzh <1049262057@qq.com>
2022-04-17 11:20:06 +0800

    ts

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export interface Options {
+  rows: number;
+  cols: number;
+}
---
+++ src/defaults.ts
@@ -0,0 +1,8 @@
+import { Options } from './config'
+
+const defaults: Options = {
+  rows: 3,
+  cols: 3
+}
+
+export default defaults
